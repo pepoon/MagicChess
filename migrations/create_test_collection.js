bc6dd2065c9692dc0bc40bae7d28d13daa168297
josep <calullmercade@gmail.com>
2021-10-12 15:41:42 +0200

    kind of sql. There is no need for mongo. But I'll use it to see the collections schemas

---
+++ migrations/create_test_collection.js
@@ -0,0 +1,12 @@
+db.test.insertOne(
+   {
+      text: 'text1',
+      number: 1,
+      array: [ 'text2', 'text3' ],
+      object: {
+         number: 2,
+         number2: 3,
+         text: 'text4'
+      }
+   },
+);
