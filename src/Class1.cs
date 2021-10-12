82d1fb9df4463134e8f84e27564b1b5535a01c25
josep <calullmercade@gmail.com>
2021-10-12 15:39:02 +0200

    initial projects

---
+++ src/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MagicChess
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/MagicChess.Logon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MagicChess.Logon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
